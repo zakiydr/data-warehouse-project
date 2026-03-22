fd67ef69c89b1bc91aad676d8659b8fde07dd219
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2026-03-22 21:23:36 +0800

    create new dev branch and commit init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,24 @@
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
